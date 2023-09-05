39e44c1ee9690b8d52ee7957ae44c1b851c62b15
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-04 17:48:09 -0700

    Add SSL/TLS configs

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,11 @@ import { MongoClient } from "mongodb";
 let db;
 
 async function connectToDb(cb){
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.mhr2urv.mongodb.net/`);
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.mhr2urv.mongodb.net/`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true, // Enable SSL/TLS
+        tlsAllowInvalidHostnames: true,);
     await client.connect();
     db = client.db('react-blog-db');
     cb();
