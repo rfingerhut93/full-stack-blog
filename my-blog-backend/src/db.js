99ae0fb5e9656e8db73f80f2e75ecb3783a36ff8
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-05 12:50:15 -0700

    Update credentials

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 let db;
 
 async function connectToDb(cb){
-    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.mhr2urv.mongodb.net/`);
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster1.hnfdbec.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
     db = client.db('react-blog-db');
     cb();
