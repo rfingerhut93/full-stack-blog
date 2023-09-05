519eb9c86226a968a4dacb0cb0d267bef9d812e7
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-05 12:01:08 -0700

    Remove depreciated MongoDB props

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -4,10 +4,8 @@ let db;
 
 async function connectToDb(cb){
     const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.mhr2urv.mongodb.net/`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         ssl: true, // Enable SSL/TLS
-        tlsAllowInvalidHostnames: true});
+});
     await client.connect();
     db = client.db('react-blog-db');
     cb();
