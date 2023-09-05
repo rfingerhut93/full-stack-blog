98b6cd0e29451fbf2242886d2bb3841c228c8fdb
Rachel Fingerhut <fingerhutrachel@gmail.com>
2023-09-04 17:43:21 -0700

    Comment out crediential code

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -19,7 +19,7 @@ import {db, connectToDb} from './db.js';
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(express.static(path.join(__dirname, '../build')));
 
 app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
